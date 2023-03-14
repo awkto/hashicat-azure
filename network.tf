4ac977495d5c014cae8e534ba9e768f867ba0f68
awkto <awkto@outlook.com>
2023-03-14 06:37:45 +0000

    Replaced depends on example, with depends on myresourcegroup

--- network.tf
+++ network.tf
@@ -26,7 +26,7 @@ module "network" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.myresourcegroup]
 }
 
 #module "network" {
